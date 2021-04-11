399cc2804dff6112476cb53e737a7c66ccc3241e
Choinamki <uyom589@gmail.com>
2021-04-11 18:28:54 +0900

    fix users model, admin

--- users/models.py
+++ users/models.py
@@ -5,4 +5,19 @@ from django.db import models
 
 
 class User(AbstractUser):
+
+    """ Custom User Model"""
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'male'),
+        (GENDER_FEMALE, 'female'),
+        (GENDER_OTHER, 'other')
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES,
+                              max_length=10, null=True, blank=True)
     bio = models.TextField(default='')
