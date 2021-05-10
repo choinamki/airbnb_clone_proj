9d79f16af3e242ca15bd9a12a3bc0a81add2b096
Choinamki <uyom589@gmail.com>
2021-05-10 20:57:20 +0900

    add rooms models save function

--- rooms/models.py
+++ rooms/models.py
@@ -82,6 +82,10 @@ class Room(core_models.TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super.save(self, *args, **kwargs)
+
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = int()
