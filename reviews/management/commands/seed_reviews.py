6bdc1eb741b3e74a495743c9e0ab04e9fffe85a2
Choinamki <uyom589@gmail.com>
2021-05-15 13:59:13 +0900

    fix typo

--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -1,7 +1,7 @@
 import random
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from reviews import models as review_modles
+from reviews import models as review_models
 from users import models as user_models
 from rooms import models as room_models
 
@@ -19,7 +19,7 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         users = user_models.User.objects.all()
         rooms = room_models.Room.objects.all()
-        seeder.add_entity(review_modles.Review, number, {
+        seeder.add_entity(review_models.Review, number, {
             'accuracy': lambda x: random.randint(0, 6),
             'communication': lambda x: random.randint(0, 6),
             'cleanliness': lambda x: random.randint(0, 6),
