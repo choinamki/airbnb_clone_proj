334f948c6177a033d3e0d1e792251a21ae246694
Choinamki <uyom589@gmail.com>
2021-05-15 13:21:05 +0900

    add seed_reviws

--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -1,6 +1,9 @@
+import random
 from django.core.management.base import BaseCommand
-from reviews.models import Review
-
+from django_seed import Seed
+from reviews import models as review_modles
+from users import models as user_models
+from rooms import models as room_models
 
 class Command(BaseCommand):
 
@@ -11,8 +14,20 @@ class Command(BaseCommand):
             '--number', type=int, default=1, help='How many reviews do you want to create'
         )
 
-
     def handle(self, *args, **options):
-
-
+        number = options.get('number')
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(review_modles.Review, number, {
+            'accuracy': lambda x: random.randint(0, 6),
+            'communication': lambda x: random.randint(0, 6),
+            'cleanliness': lambda x: random.randint(0, 6),
+            'location': lambda x: random.randint(0, 6),
+            'check_in': lambda x: random.randint(0, 6),
+            'value': lambda x: random.randint(0, 6),
+            'room': lambda x: random.choice(rooms),
+            'user': lambda x: random.choice(users),
+        })
+        seeder.execute()
         self.stdout.write(self.style.SUCCESS('Reviews created!'))
