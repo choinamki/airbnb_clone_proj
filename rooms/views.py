586cf279362a0c6167480ae1279735e5c8502cc2
Choinamki <uyom589@gmail.com>
2021-05-16 19:29:04 +0900

    fix paginator using django paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,12 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get('page', 1))
-    page = page or 1
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    page_count = ceil(models.Room.objects.count() / page_size)
-    rooms_all = models.Room.objects.all()[offset:limit]
-    return render(request, 'rooms/home.html',
-                  {'rooms': rooms_all, 'page': page, 'page_count': page_count,
-                   'page_range': range(1, page_count)})
+    page = request.GET.get('page')
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, 'rooms/home.html', {'rooms': rooms})
