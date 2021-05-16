89f614f356495475641d87f47323c9b1cd83a4c4
Choinamki <uyom589@gmail.com>
2021-05-16 17:26:25 +0900

    (fix rooms view and home.html) about page

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,16 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
     page = int(request.GET.get('page', 1))
+    page = page or 1
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
-    print(offset)
-    print(limit)
-    all_rooms = models.Room.objects.all()[offset:limit]
-    return render(request, 'rooms/home.html', {'rooms': all_rooms})
+    page_count = ceil(models.Room.objects.count() / page_size)
+    rooms_all = models.Room.objects.all()[offset:limit]
+    return render(request, 'rooms/home.html',
+                  {'rooms': rooms_all, 'page': page, 'page_count': page_count,
+                   'page_range': range(1, page_count)})
