98036643f1372b0e2dfb6848b211d37c1a79bccb
Choinamki <uyom589@gmail.com>
2021-05-19 21:24:31 +0900

    add html from about room search

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
+from django_countries import countries
 from . import models
 
 
@@ -25,6 +26,7 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get('city')
+    city = request.GET.get('city', 'Anywhere')
     city = str.capitalize(city)
-    return render(request, 'rooms/search.html', {'city': city})
+    room_types = models.RoomType.objects.all()
+    return render(request, 'rooms/search.html', {'city': city, 'countries': countries, 'room_types': room_types})
