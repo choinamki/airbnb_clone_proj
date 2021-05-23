edde49367ce41cac267fe867c031b82699a022f9
Choinamki <uyom589@gmail.com>
2021-05-23 22:35:02 +0900

    remove login function veiw comment

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
+from . import models
 
 
 class LoginView(FormView):
@@ -21,21 +22,6 @@ class LoginView(FormView):
                 login(self.request, user)
                 return super().form_valid(form)
         return redirect(reverse('core:home'))
-    # def get(self, request):
-    #     form = forms.LoginForm()
-    #     return render(request, 'users/login.html', {'form': form})
-    #
-    # def post(self, request):
-    #     form = forms.LoginForm(request.POST)
-    #     if form.is_valid():
-    #         email = form.cleaned_data.get('email')
-    #         password = form.cleaned_data.get('password')
-    #         user = authenticate(request, username=email, password=password)
-    #         if user is not None:
-    #             login(request, user)
-    #             return redirect(reverse('core:home'))
-    #     return render(request, 'users/login.html', {'form': form})
-
 
 def log_out(request):
     logout(request)
@@ -59,4 +45,5 @@ class SignUpView(FormView):
         user = authenticate(self.request, username=email, password=password)
         if user is not None:
             login(self.request, user)
+        user.verify_email()
         return super().form_valid(form)
