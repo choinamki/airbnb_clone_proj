1b20679a4873bd47a1a5f7c94441bc71bafde19e
choinamki <uyom589@gmail.com>
2021-06-03 09:35:13 +0900

    fix calender func, add calendar css

--- cal.py
+++ cal.py
@@ -7,7 +7,7 @@ class Calendar(calendar.Calendar):
         super().__init__(firstweekday=6)
         self.year = year
         self.month = month
-        self.day_names = ('Mon', 'Tue', 'Wed', 'Fri', 'Sat', 'Sun')
+        self.day_names = ('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
         self.months = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
                        'August', 'September', 'October', 'November', 'December')
 
